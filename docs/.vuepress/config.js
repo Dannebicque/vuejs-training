0164acf70e55a1016f2914348ea9ed2c09682022
davidannebicque <david.annebicque@gmail.com>
2024-10-23 14:36:45 +0200

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,58 +1,58 @@
 module.exports = {
 	base: "/",
 	locales: {
-		"/": {
+		"/en": {
 			lang: "en-US", // this will be set as the lang attribute on <html>
 			title: "Introduction to Vue.js",
 			description: "Discover Vue.js framework through this 2-days training",
 		},
 
-		"/fr/": {
+		"/": {
 			lang: "fr-FR",
 			title: "Introduction à Vue.js",
-			description: "Découvrez le framework Vue.js dans cette formation de 2 jours",
+			description: "Découvrez le framework Vue.js version 3",
 		},
 	},
 
 	themeConfig: {
 		locales: {
-			"/": {
+			"/en/": {
 				selectText: "Language",
 				label: "English",
 				sidebar: [
-					"/",
-					"/presentation/",
-					"/tooling/",
-					"/views/",
-					"/directives/",
-					"/components/",
-					"/reactivity/",
-					"/state/",
-					"/routing/",
-					"/http/",
-					"/tests/",
-					"/reusability/",
-					"/ecosystem/",
+					"/en/",
+					"/en/presentation/",
+					"/en/tooling/",
+					"/en/views/",
+					"/en/directives/",
+					"/en/components/",
+					"/en/reactivity/",
+					"/en/state/",
+					"/en/routing/",
+					"/en/http/",
+					"/en/tests/",
+					"/en/reusability/",
+					"/en/ecosystem/",
 				],
 			},
 
-			"/fr/": {
+			"/": {
 				selectText: "Langue",
 				label: "Français",
 				sidebar: [
-					"/fr/",
-					"/fr/presentation/",
-					"/fr/outillage/",
-					"/fr/vues/",
-					"/fr/directives/",
-					"/fr/composants/",
-					"/fr/reactivite/",
-					"/fr/etat/",
-					"/fr/routage/",
-					"/fr/http/",
-					"/fr/tests/",
-					"/fr/reutilisabilite/",
-					"/fr/ecosysteme/",
+					"/",
+					"/presentation/",
+					"/outillage/",
+					"/vues/",
+					"/directives/",
+					"/composants/",
+					"/reactivite/",
+					"/etat/",
+					"/routage/",
+					"/http/",
+					"/tests/",
+					"/reutilisabilite/",
+					"/ecosysteme/",
 				],
 			},
 		},
